a43688f7d50a86777b36c0586af15f2aef4d9603
Patrick Reiter Horn <patrick.horn@gmail.com>
2025-02-27 15:39:58 -0800

    Support request cancellation

--- build_python_changes.sh
+++ build_python_changes.sh
@@ -1,3 +1,3 @@
 git diff 20d629e1b1505f41d0f7bcf4edca2c02dc27e9ed -- tensorrt_llm > python.patch
 
-docker build -t localhost:30500/tensorrt-llm:"$1" --build-arg url=localhost:30500/tensorrt-llm-builder:feb25 . -f Dockerfile.python
+docker build -t localhost:30500/tensorrt-llm:"$1" --build-arg url=localhost:30500/tensorrt-llm-builder:"$2" . -f Dockerfile.python
