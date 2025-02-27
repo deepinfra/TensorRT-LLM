9aba68c4fc7c091a13d8a1a443c153777a7fa5bf
Patrick Reiter Horn <patrick.horn@gmail.com>
2025-02-26 18:13:22 -0800

    Use native set() for self.canceled_req_ids because ReqIdsSet is not pickleable

--- tensorrt_llm/_torch/pyexecutor/py_executor.py
+++ tensorrt_llm/_torch/pyexecutor/py_executor.py
@@ -220,7 +220,7 @@ class PyExecutor:
         self.send_new_reqs_handle = [None] * 2
 
         self.inflight_req_ids = ReqIdsSet()
-        self.canceled_req_ids = ReqIdsSet()
+        self.canceled_req_ids = set()
 
         self.model_engine.warmup(self.resource_manager)
         if self.draft_model_engine is not None:
@@ -301,7 +301,7 @@ class PyExecutor:
         Args:
             id (int): The request id for which to cancel the response
         """
-        self.canceled_req_ids.insert(id)
+        self.canceled_req_ids.add(id)
 
     def shutdown(self):
         """
@@ -1225,7 +1225,7 @@ class PyExecutor:
                 req.is_dummy = False
                 self.active_requests.append(req)
             elif _is_cancel_request(req_item):
-                self.canceled_req_ids.insert(req_item)
+                self.canceled_req_ids.add(req_item)
 
         return False
 
@@ -1350,7 +1350,7 @@ class PyExecutor:
                 req.is_dummy = False
                 self.active_requests.append(req)
             elif _is_cancel_request(req_item):
-                self.canceled_req_ids.insert(req_item)
+                self.canceled_req_ids.add(req_item)
 
         return False
 
@@ -1809,7 +1809,7 @@ class PyExecutor:
                 cancelled_responses[req_id] = request.create_response(
                     False, self.dist.rank)
                 self.canceled_requests.append(request)
-                self.canceled_req_ids.erase(req_id)
+                self.canceled_req_ids.remove(req_id)
             else:
                 left_requests.append(request)
         self.active_requests = left_requests
