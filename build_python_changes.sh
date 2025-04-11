36537b12d9fde36d8234e3ad2115cfb79cd62d30
Patrick Reiter Horn <patrick.horn@gmail.com>
2025-04-11 19:43:19 +0000

    update build script revision

--- build_python_changes.sh
+++ build_python_changes.sh
@@ -1,3 +1,3 @@
-git diff 5262f3c1056469133284e4faeee936979d298d9c -- tensorrt_llm > python.patch
+git diff 3058fdb97f3518933c7fab4260f599c1718da45e -- tensorrt_llm > python.patch
 
 docker build -t localhost:30500/tensorrt-llm:"$1" --build-arg url=localhost:30500/tensorrt-llm-builder:"$2" . -f Dockerfile.python
