b727da385c040749ca3e44aa5bce6ff5579afe58
Shang-Pin <shang-pin@deepinfra.com>
2025-08-01 23:59:56 +0000

    patch

--- build_python_changes.sh
+++ build_python_changes.sh
@@ -1,3 +1,3 @@
-git diff 63a8c28e70cc59fcb363d823476376ee8de8a3af -- tensorrt_llm > python.patch
+git diff 2d21bca25e60a6d9cfe31903c75cde29d6111145 -- tensorrt_llm > python.patch
 
 docker build -t localhost:30500/pytrtllm:"$1" --build-arg url=localhost:30500/pytrtllm-builder:"$2" . -f Dockerfile.python
