912be74caf99d327b1df9426452bc5746b701723
Shang-Pin Sheng <shang-pin@deepinfra.com>
2025-11-05 11:25:11 -0800

    fix

--- tensorrt_llm/serve/postprocess_handlers.py
+++ tensorrt_llm/serve/postprocess_handlers.py
@@ -64,34 +64,20 @@ class ChatPostprocArgs(PostprocArgs):
             top_logprobs=bool(request.top_logprobs),
         )
 
-
-def create_logprobs(token_ids: List[int], tokenizer: TransformersTokenizer,
-                    logprobs: List[float] | TokenLogprobs,
+def create_logprobs(token_ids: List[int],
+                    tokenizer: TransformersTokenizer,
+                    logprobs: TokenLogprobs,
                     top_logprobs: bool) -> ChatCompletionLogProbs:
-    assert len(token_ids) == len(logprobs), \
-            "token_ids and logprobs have different lengths"
     content: List[ChatCompletionLogProbsContent] = []
-    for token_id, logprob in zip(token_ids, logprobs):
-        logprob: float | dict[int, Logprob]
+    for logprob in logprobs:
+        token_id, lp = list(logprob.items())[0]
         token = tokenizer.decode(token_id)
-        chat_logprob = ChatCompletionLogProbsContent(
+        # returning multiple logprobs is not supported
+        first_logprob = ChatCompletionLogProbsContent(
             token=token,
-            bytes=list(token.encode("utf-8", errors="replace")),
-        )
-        if isinstance(logprob, dict):
-            if token_id in logprob:
-                chat_logprob.logprob = max(logprob[token_id].logprob, -9999.0)
-                if top_logprobs:
-                    chat_logprob.top_logprobs = [
-                        ChatCompletionLogProbsContent(
-                            token=(tk := tokenizer.decode(tid)),
-                            logprob=max(logprob.logprob, -9999.0),
-                            bytes=list(tk.encode("utf-8", errors="replace")))
-                        for tid, logprob in logprob.items()
-                    ]
-        else:
-            chat_logprob.logprob = max(logprob, -9999.0)
-        content.append(chat_logprob)
+            logprob=max(lp.logprob, -9999.0),
+            bytes=list(token.encode("utf-8", errors="replace")))
+        content.append(first_logprob)
     chat_logprobs = ChatCompletionLogProbs(content=content)
     return chat_logprobs
 
@@ -108,7 +94,6 @@ def create_logprobs_completion(token_ids: List[int],
         tokens.append(token)
     completion_logprobs = CompletionLogProbs(token_logprobs=token_logprobs,tokens=tokens)
     return completion_logprobs
-
 
 def apply_reasoning_parser(args: ChatPostprocArgs, output_index: int, text: str,
                            streaming: bool) -> Tuple[bool, str, str]:
