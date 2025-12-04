52364464a0f36beb48f019785e0e48e4278e78b6
Shang-Pin <shang-pin@deepinfra.com>
2025-12-04 22:34:49 +0000

    build version & fast-hadamard-transform

--- build_python_changes.sh
+++ build_python_changes.sh
@@ -1,3 +1,3 @@
-git diff 9ec01e6afb800cbedc0dac42cf77b23d9bda36d9 -- tensorrt_llm > python.patch
+git diff 4e8087f71783518f4b1b915dbd6c5e3e6a52abce -- tensorrt_llm > python.patch
 
 docker build -t localhost:30500/pytrtllm:"$1" --build-arg url=localhost:30500/pytrtllm-builder:"$2" . -f Dockerfile.python
