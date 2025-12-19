807e04c5433534b6f76b70434616e700acce0dd1
Pernekhan Utemuratov <pernekhan@deepinfra.com>
2025-12-19 17:06:43 +0000

    Change has for building python

--- build_python_changes.sh
+++ build_python_changes.sh
@@ -1,3 +1,3 @@
-git diff 64513797f2055d4c58a12f0328bb7ea72a104758 -- tensorrt_llm > python.patch
+git diff 4b4547c703138c26b560ac776d992fcd17496b6f -- tensorrt_llm > python.patch
 
 docker build -t localhost:30500/pytrtllm:"$1" --build-arg url=localhost:30500/pytrtllm-builder:"$2" . -f Dockerfile.python
