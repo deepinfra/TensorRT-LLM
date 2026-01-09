14934f7babc08f5d1d747ba8a525ccb3598a3e13
Pernekhan Utemuratov <pernekhan@deepinfra.com>
2026-01-09 21:44:18 +0000

    Change hash for pytrtllm-builder

--- build_python_changes.sh
+++ build_python_changes.sh
@@ -1,3 +1,3 @@
-git diff 64513797f2055d4c58a12f0328bb7ea72a104758 -- tensorrt_llm > python.patch
+git diff 08785bd44f2838b562cc292907a6a8ee65182c4b -- tensorrt_llm > python.patch
 
 docker build -t localhost:30500/pytrtllm:"$1" --build-arg url=localhost:30500/pytrtllm-builder:"$2" . -f Dockerfile.python
