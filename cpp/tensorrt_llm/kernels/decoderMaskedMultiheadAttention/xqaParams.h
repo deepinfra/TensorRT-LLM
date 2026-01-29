a1631c267d846d5ea23f8b9e37546bca6ab772b9
Shang-Pin Sheng <shang-pin@deepinfra.com>
2026-01-29 13:31:24 -0800

    fix isMLA

--- cpp/tensorrt_llm/kernels/decoderMaskedMultiheadAttention/xqaParams.h
+++ cpp/tensorrt_llm/kernels/decoderMaskedMultiheadAttention/xqaParams.h
@@ -215,7 +215,7 @@ struct XQAParams
 
     bool isMLA() const
     {
-        return head_size == 576 && num_q_heads == 128 && num_kv_heads == 1;
+        return head_size == 576 && num_kv_heads == 1;
     }
 };
 
