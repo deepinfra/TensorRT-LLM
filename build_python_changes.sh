8acf94d63efa95f1fd150eb8868edd1614d0c41d
Thach Nguyen <thach@deepinfra.com>
2026-03-04 19:53:26 +0000

    update script

--- build_python_changes.sh
+++ build_python_changes.sh
@@ -1,3 +1,3 @@
-git diff e3ed846bb2a851c06b2f2e2527c51157be60b2a6 -- tensorrt_llm > python.patch
+git diff pytrtllm-builder/$2 -- tensorrt_llm > python.patch
 
 docker build -t localhost:30500/pytrtllm:"$1" --build-arg url=localhost:30500/pytrtllm-builder:"$2" . -f Dockerfile.python
